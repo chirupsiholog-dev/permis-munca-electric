ca66a2bddae150e717d4404d6e30f12f4ad45d0b
Adamita David Stefan <adamita.david@gmail.com>
2026-07-29 12:38:45 +0300

    Added JWT login

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -1,29 +1,38 @@
 import type { Request, Response } from 'express';
-import { supabase } from '../supabaseClient.js';
+import { supabase } from '../lib/supabaseClient.js';
 import crypto from 'node:crypto';
+import jwt from 'jsonwebtoken'
 
 export const login = async(req: Request, res: Response) => {
 
     const {email, password} = req.body;
 
     if(!email || !password){
-        res.status(401).json({'error': 'Credentials are mandatory'});
+        return res.status(401).json({'error': 'Credentials are mandatory'});
     }
 
     const {data, error} = await supabase.from('users').select('id, password_hash').eq('email', email).maybeSingle();
 
     if(error){
-        res.status(500).json({'error': `DB Error: ${error.message}`});
+        return res.status(500).json({'error': `DB Error: ${error.message}`});
     }
 
     if(!data){
-        res.status(400).json({'error': 'User not found'});
+       return res.status(400).json({'error': 'User not found'});
     }
 
     if(crypto.hash('sha256', password) !== data?.password_hash){
-        res.status(401).json({'error': 'Invalid credentials'});
+        return res.status(401).json({'error': 'Invalid credentials'});
     }
 
+    const secretKey = process.env.JWT_SECRET;
 
+    if(!secretKey){
+        console.log('JWT Secret not loaded from env');
+        throw new Error('JWT Secret not loaded from env');
+    }
+
+    const token = jwt.sign({userId: data?.id}, secretKey, {expiresIn: '1d'}); //expiresIn is injected into the {userId: data.id} payload
+    return res.status(200).json({'success': true, 'token': token});
 
 }
--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,7 @@ const app = express();
 app.use(express.json()); //middleware for parsing json
 app.use(express.urlencoded({ extended: true }));
 
-app.use('/auth', authRoutes);
+app.use('/api/auth', authRoutes);
 
 app.listen(3000, () => {
     console.log('Server is running on port 3000');
