a6741434f68176e972b213d37acaf6bb60948039
Adamita David Stefan <adamita.david@gmail.com>
2026-09-17 13:35:35 +0300

    Photos

--- src/controllers/inventarController.ts
+++ src/controllers/inventarController.ts
@@ -2,6 +2,8 @@ import type { Request, Response } from "express";
 import { supabase } from "../lib/supabaseClient.js";
 import { fillInventarPdf, type InventarData } from "../lib/utils.js";
 import path from "path";
+import crypto from 'node:crypto'
+import multer from "multer";
 
 interface Inventar{
 
@@ -21,7 +23,36 @@ interface Inventar{
     data: string,
     inverter: string,
     turnon: string,
-    turnoff: string
+    turnoff: string,
+    existingImages: string
+}
+
+async function uploadPhotoWithRetry(photo: Express.Multer.File, uniqueFileName: string, inverter: string, data: string, noRetries: number){
+
+    for(let i = 0; i < noRetries; i++){
+
+        try{
+             //upload each photo to the bucket
+            const {error: storageError} = await supabase.storage.from('Images').upload(uniqueFileName, photo.buffer, {
+                contentType: photo.mimetype,
+                upsert: true
+            })
+            if(storageError){
+               throw new Error(storageError.message)
+            }
+
+            const {data: storageUrl} = await supabase.storage.from('Images').getPublicUrl(uniqueFileName)
+            if(!storageUrl){
+                throw new Error('Failed to extract storageUrl')
+            }
+
+            return storageUrl.publicUrl
+        }catch(err){
+            console.log(err)
+        }
+    }
+
+    return null;
 }
 
 function isValidInventar(body: any): body is Inventar{
@@ -68,6 +99,26 @@ export const uploadInventar = async (req: Request, res: Response) => {
     suruburi,
     remarks, inverter, data, turnon, turnoff} = req.body
 
+    const photos = req.files as Express.Multer.File[]
+
+    const photosUrls = [];
+    let failedPhotos = 0;
+
+    for(const photo of photos ?? []){
+
+        const uniqueFileName = `poza_checklist_${inverter}_${data}_${crypto.randomUUID()}`
+        const storageUrl = await uploadPhotoWithRetry(photo, uniqueFileName, inverter, data, 3)
+        if(!storageUrl)
+            failedPhotos++;
+        else
+            photosUrls.push({path: uniqueFileName, url: storageUrl})
+    }
+
+    if(failedPhotos !== 0){
+        await supabase.storage.from('Images').remove(photosUrls.map(p => p.path))
+        return res.status(400).json({error: `${failedPhotos} photos failed to upload`})
+    }
+
     const {error: inventarError} = await supabase.from('inventare').insert({
         'user_id': userId,
         'praf': praf,
@@ -86,11 +137,14 @@ export const uploadInventar = async (req: Request, res: Response) => {
         'inverter': inverter.trim(),
         'data': data,
         'turn_on': turnon.trim(),
-        'turn_off': turnoff.trim()
+        'turn_off': turnoff.trim(),
+        'images': photosUrls
     })
 
-    if(inventarError)
+    if(inventarError){
+        await supabase.storage.from('Images').remove(photosUrls.map(p => p.path))
         return res.status(500).json({error: 'Internal server error'});
+    }
 
     return res.status(200).json({success: true, message: 'Inventar salvat cu succes'})
 
@@ -152,8 +206,63 @@ export const editInventar = async(req: Request, res: Response) => {
     impamantare,
     comutatorCurent,
     suruburi,
-    remarks, inverter, data, turnon, turnoff} = req.body
+    remarks, inverter, data, turnon, turnoff, existingImages} = req.body
 
+    const newPhotos = req.files as Express.Multer.File[]
+
+    let existingPhotos = []
+    //parse existingImages coming from the request body, as it comes as a JSON string
+    try{
+        const raw = existingImages
+        existingPhotos = raw ? JSON.parse(raw): []
+    }catch{
+        return res.status(400).json({error: 'Imagini existente invalide'})
+    }
+
+    if (!Array.isArray(existingPhotos)) {
+        return res.status(400).json({ error: 'Imagini existente invalide' })
+    }
+
+    //see what photos are already stored
+    const {data: photosInDb, error: photosinDbError} = await supabase.from('inventare').select('images').eq('id', inventarId).eq('user_id', userId).maybeSingle();
+    if(photosinDbError){
+        return res.status(500).json({error: 'Internal Server Error'})
+    }
+
+    const oldImages = Array.isArray(photosInDb?.images)
+        ? photosInDb.images
+        : []
+
+    //get the storage paths of the photos that remained in the frontend in a set
+    const existingImagesStoragePaths = new Set(existingPhotos.filter((image) => typeof image.path === 'string').map(img => img.path))
+
+    //see what photos should be kept
+    const retainedImages = oldImages.filter((image) =>
+        existingImagesStoragePaths.has(image.path),
+        )
+    //see what photos were not in the ones sent by the frontend keep them for deletion
+    const photosToDelete = oldImages.filter((photo: {path: string, url: string}) => !existingImagesStoragePaths.has(photo.path))
+
+    //upload to storage the new photos uploaded by the user
+    let failedPhotos = 0;
+    const newPhotosUrls = [];
+
+    for(const photo of newPhotos){
+        const uniqueFileName = `poza_checklist_${inverter}_${data}_${crypto.randomUUID()}`
+        const storageUrl = await uploadPhotoWithRetry(photo, uniqueFileName, inverter, data, 3)
+        if(!storageUrl)
+            failedPhotos++;
+        else
+            newPhotosUrls.push({path: uniqueFileName, url: storageUrl})
+    }
+    //if an upload failed
+    if(failedPhotos !== 0){
+        //remove all new added photos from the storage - all or nothing
+        await supabase.storage.from('Images').remove(newPhotosUrls.map(p => p.path))
+        return res.status(400).json({error: `${failedPhotos} photos failed to upload`})
+    }
+
+    //update the db row
     const {error: inventarUpdateError} = await supabase.from('inventare').update({
         'praf': praf,
         'ventilatoare': ventilatoare,
@@ -171,11 +280,20 @@ export const editInventar = async(req: Request, res: Response) => {
         'inverter': inverter.trim(),
         'data': data,
         'turn_on': turnon.trim(),
-        'turn_off': turnoff.trim()
+        'turn_off': turnoff.trim(),
+        'images': [...retainedImages, ...newPhotosUrls] //what the frontend tells us remained + the new uploaded photos
     }).eq('id', inventarId).eq('user_id', userId)
 
+    //if there was an update error
     if(inventarUpdateError)
+    {
+        //remove the newly added photos from storage - as the updated failed, they will not be saved in the DB row, so they do not need to be left in the storage
+        await supabase.storage.from('Images').remove(newPhotosUrls.map(p => p.path))
         return res.status(500).json({error: 'Internal server error'});
+    }
+
+    //finally, delete from to storage the images the user deleted
+    await supabase.storage.from('Images').remove(photosToDelete.map(p => p.path))
 
     return res.status(200).json({success: true, message: 'Inventar editat cu succes'})
 
@@ -195,6 +313,9 @@ export const deleteInventar = async(req: Request, res: Response)=>{
     if(!checkInventarData){
         return res.status(404).json({error: 'Inventar invalid'});
     }
+
+    //delete images from storage first
+    await supabase.storage.from('Images').remove(checkInventarData.images.map((p: {path: string, url: string}) => p.path))
 
     const {error: deleteInventarError} = await supabase.from('inventare').delete().eq('user_id', userId).eq('id', inventarId);
     if(deleteInventarError)
