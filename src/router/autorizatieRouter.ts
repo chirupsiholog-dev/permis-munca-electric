5330844fe62357692f9ac084e976fb2a6fa803aa
Luca David <robertlucadavid@gmail.com>
2026-09-24 09:52:18 +0300

    embedding jpg and png to autorizatii pdf

--- src/router/autorizatieRouter.ts
+++ src/router/autorizatieRouter.ts
@@ -1,10 +1,13 @@
 import Router from 'express'
 import { getCurrentUser } from '../middleware/getCurrentUser.js'
 import { requireAdmin } from '../middleware/requireAdmin.js'
-import { postAutorizatie } from '../controllers/autorizatieController.js'
+import { createPdfWithImages, getAllAutorizatii, postAutorizatie } from '../controllers/autorizatieController.js'
+import { uploadImage } from '../middleware/uploadImage.js'
 
 const router = Router()
 
-router.post('/new', getCurrentUser, requireAdmin, postAutorizatie)
+router.post('/new', getCurrentUser, requireAdmin, postAutorizatie);
+router.post('/pdf-with-images', requireAdmin, uploadImage.single('image'), createPdfWithImages);
+router.get('/all', getCurrentUser, requireAdmin, getAllAutorizatii);
 
 export default router
